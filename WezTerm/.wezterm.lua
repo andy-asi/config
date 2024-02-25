6f3eedc8be8ee5a94f4a74b735ab894f1035e637
Andy Asi <andyasi@Andys-iMac.local>
2024-02-25 18:16:58 +1100

    add wezterm config

---
+++ WezTerm/.wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.font_size = 16
+config.color_scheme = "Dracula (Official)"
+config.tab_bar_at_bottom = true
+-- config.use_fancy_tab_bar = false
+-- config.window_decorations = "RESIZE"
+config.font = wezterm.font_with_fallback({ "Fira Code", "JetBrainsMono Nerd Font" })
+config.initial_rows = 56
+config.initial_cols = 200
+
+-- and finally, return the configuration to wezterm
+return config
